064801130278df56758d59f16677f747aaee1756
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-03-18 23:34:27 +0545

    users list and signin endpoint

--- ddl.sql
+++ ddl.sql
@@ -1,3 +1,15 @@
 CREATE TABLE users(
-    name varchar2;
-)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(63) NOT NULL,
+    email VARCHAR(63) NOT NULL UNIQUE,
+    available BOOLEAN DEFAULT true,
+    password VARCHAR(72) NOT NULL
+);
+
+CREATE TABLE companies(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(63) NOT NULL,
+    email VARCHAR(63) NOT NULL UNIQUE,
+    openings SMALLINT CHECK(openings >= 0),
+    password VARCHAR(72) NOT NULL
+);
--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 // Modules import
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const db = require("./db");
 
 // Middlewares
 const app = express();
+app.use(bodyParser());
 app.use(cors());
 
 // Routes
-app.get("/", (req, res) => {
-    // db.query("select * from ");
-    res.sendFile(__dirname + "/client/index.html");
+app.post("/signin", async (req, res) => {
+    try {
+        res.send(req);
+        var { name, email, password } = req.body;
+        var query = `insert into users values(default, '${name}', '${email}', default, '${password}');`;
+        res.send(query);
+        var result = await db.query(query);
+        res.status(201).send(result);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+app.get("/users", async (req, res) => {
+    try {
+        var { rowCount, rows } = await db.query(
+            "select id, name, email, available from users"
+        );
+        res.status(200).send({ userCount: rowCount, users: rows });
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
 
 app.get("/greet", (req, res) => {
