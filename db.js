cb5ed7dfc8fb42f926bc10f85831d5250aca0f4c
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-03-19 16:55:34 +0545

    Login endpoint created

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const { Pool } = require("pg");
+if (process.env.NODE_ENV !== "procuction") {
+    require("dotenv").config();
+}
 
 const pool = new Pool({
     user: "postgres",
-    password: "gunjan",
+    password: process.env.PASSWORD,
     host: "localhost",
     port: "5432",
     database: "fourspace",
--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 // Modules import
+if (process.env.NODE_ENV !== "procuction") {
+    require("dotenv").config();
+}
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+
 const db = require("./db");
-const { urlencoded } = require("express");
 
 // Express Middlewares
 const app = express();
 app.use(express.json());
-app.use(urlencoded({ extended: false }));
 app.use(cors());
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+// Custom Middlewares
+function isAuthenticated(req, res, next) {
+    return next();
+}
+
+function errmsg(msg) {
+    return { error: msg };
+}
 
 // Routes
-app.get("/", (req, res) => {
-    res.send("Goto /signin");
-});
-
-app.post("/register", async (req, res) => {
+app.get("/login", async (req, res) => {
     try {
-        var { name, email, password, type } = req.body;
+        const { email, password, type } = req.body;
+
         var table;
         if (type == "user") {
             table = "users";
@@ -27,15 +44,45 @@ app.post("/register", async (req, res) => {
             res.status(400).send({ error: "Invalid type" });
         }
 
-        var query = `insert into ${table} values(default, '${name}', '${email}', default, '${password}');`;
-        var result = await db.query(query);
+        var query = `select password from ${table} where email = ${email};`;
+        var hashedPassword = await db.query(query);
         res.status(200).send(result);
     } catch (err) {
-        res.status(500).send(err);
+        res.status(500).json({ error: err.detail });
     }
 });
 
-app.get("/users", async (req, res) => {
+// Register Users
+app.post("/register", async (req, res) => {
+    try {
+        var { name, email, password, type } = req.body;
+
+        if (password.length < 4) {
+            res.status(400).send(errmsg("Password too short"));
+        }
+
+        var table;
+        if (type == "user") {
+            table = "users";
+        } else if (type == "company") {
+            table = "companies";
+        } else {
+            res.status(400).send({ error: "Invalid type" });
+        }
+
+        var hashedPassword = await bcrypt.hash(password, 10);
+        res.send(hashedPassword);
+
+        var query = `insert into ${table} values(default, '${name}', '${email}', default, '${hashedPassword}');`;
+        var result = await db.query(query);
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(500).json({ error: err.detail });
+    }
+});
+
+// Get users
+app.get("/", async (req, res) => {
     try {
         var { rowCount, rows } = await db.query(
             "select id, name, email, available from users"
@@ -46,6 +93,7 @@ app.get("/users", async (req, res) => {
     }
 });
 
+// Get companies
 app.get("/companies", async (req, res) => {
     try {
         var { rowCount, rows } = await db.query(
@@ -58,6 +106,6 @@ app.get("/companies", async (req, res) => {
 });
 
 // Listening to the server
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server running at 8080...");
 });
