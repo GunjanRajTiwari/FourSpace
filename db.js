5a3c64a9e3e9a2ca7f5cc0e744ac3538b51aed40
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-03-24 00:12:13 +0545

    Postgres db hosting setup

--- db.js
+++ db.js
@@ -1,14 +1,22 @@
-const { Pool } = require("pg");
+const { Pool, Client } = require("pg");
 if (process.env.NODE_ENV !== "procuction") {
     require("dotenv").config();
 }
 
-const pool = new Pool({
-    user: "postgres",
-    password: process.env.PASSWORD,
-    host: "localhost",
-    port: "5432",
-    database: "fourspace",
+// const pool = new Pool({
+//     user: "postgres",
+//     password: process.env.PASSWORD,
+//     host: "localhost",
+//     port: "5432",
+//     database: "fourspace",
+// });
+
+var client = new Client(process.env.DBURL);
+client.connect(function (err) {
+    if (err) {
+        return console.error("could not connect to postgres", err);
+    }
+    console.log("DB Connected...");
 });
 
-module.exports = pool;
+module.exports = client;
